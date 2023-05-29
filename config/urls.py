c7c2a681c1a147c1188224f8d4e918773fdbf6f1
Zinovkin Artem <0968064971@gmail.com>
2023-05-29 11:38:06 +0300

    Pokeapi is integrated

--- config/urls.py
+++ config/urls.py
@@ -1,22 +1,103 @@
-"""
-URL configuration for config project.
+import json
+from dataclasses import asdict, dataclass
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+import requests
+from django.conf import settings
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+
+
+def filter_by_keys(sourse, keys):
+    filtered_data = {}
+
+    for key, value in sourse.items():
+        if key in keys:
+            filtered_data[key] = value
+
+    return filtered_data
+
+
+@dataclass
+class Pokemon:
+    id: int
+    name: str
+    height: int
+    weight: int
+    base_experience: int
+
+    @classmethod
+    def from_raw_data(cls, raw_data):
+        filtered_data = filter_by_keys(
+            raw_data,
+            cls.__dataclass_fields__.keys(),
+        )
+
+        return cls(**filtered_data)
+
+
+POKEMONS = {}
+
+
+def get_pokeapi_from_api(name):
+    url = settings.POKEAPI_BASE_URL + f"/{name}"
+    response = requests.get(url)
+    raw_data = response.json()
+
+    return Pokemon.from_raw_data(raw_data)
+
+
+def _get_pokemon(name):
+    if name in POKEMONS:
+        pokemon = POKEMONS[name]
+    else:
+        pokemon = get_pokeapi_from_api(name=name)
+        POKEMONS[name] = pokemon
+
+    return pokemon
+
+
+def _del_pokemon(name):
+    del POKEMONS[name]
+
+
+@csrf_exempt
+def get_pokemon(request, name):
+    if request.method == "GET":
+        pokemon = _get_pokemon(name)
+
+        return HttpResponse(
+            content_type="application/json",
+            content=json.dumps(asdict(pokemon)),
+        )
+
+    elif request.method == "DELETE":
+        _del_pokemon(name)
+        return HttpResponse(f"{name} DELETE")
+
+
+@csrf_exempt
+def get_pokemon_for_mobile(request, name):
+    if request.method == "GET":
+        pokemon = _get_pokemon(name)
+
+        result = filter_by_keys(
+            asdict(pokemon),
+            ["id", "name", "base_experience"],
+        )
+
+        return HttpResponse(
+            content_type="application/json",
+            content=json.dumps(result),
+        )
+    elif request.method == "DELETE":
+        _del_pokemon(name)
+        return HttpResponse(f"{name} DELETE")
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/pokemon/<str:name>/", get_pokemon),
+    path("api/pokemon/mobile/<str:name>/", get_pokemon_for_mobile),
 ]
